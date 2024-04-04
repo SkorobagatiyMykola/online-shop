0365d276c3ce155c0411bc8146ba737dfafa2b8a
SkorobagatiyMykola <nick1980457@gmail.com>
2024-04-04 18:30:24 +0300

    Update controller

--- src/main/java/ua/skorobahatyi/onlineshop/controller/TestController.java
+++ src/main/java/ua/skorobahatyi/onlineshop/controller/TestController.java
@@ -1,14 +1,20 @@
 package ua.skorobahatyi.onlineshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/v1/test")
 public class TestController {
     @GetMapping
     public String checkApp(){
         return "Hello Nick, how are you?";
+    }
+
+    @GetMapping("/html")
+    public String checkHtmlPage(){
+        return "hello1";
     }
 }
