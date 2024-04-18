bee27c53f19bf6facbe9c887a2d3febc3fb7a97d
SkorobagatiyMykola <nick1980457@gmail.com>
2024-04-18 19:21:55 +0300

    Changed Product and ProductController

--- src/main/java/ua/skorobahatyi/onlineshop/controller/ProductController.java
+++ src/main/java/ua/skorobahatyi/onlineshop/controller/ProductController.java
@@ -1,4 +1,24 @@
 package ua.skorobahatyi.onlineshop.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.skorobahatyi.onlineshop.service.ProductService;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/v1/products")
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public String getAllProducts(Model model) {
+        System.out.println("============================");
+        model.addAttribute("products", productService.getAllProducts());
+
+        return "products/allProducts";
+    }
+
 }
--- src/main/java/ua/skorobahatyi/onlineshop/entity/Product.java
+++ src/main/java/ua/skorobahatyi/onlineshop/entity/Product.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
+//@Data
 @Entity(name = "products")
 public class Product {
     @Id
